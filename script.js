cf5b4286b0a7416f7e520c0ed89454f0a000d343
Ben Scarletti <scarletti.ben@gmail.com>
2025-12-29 13:51:36 +0000

    significant UI overhaul

--- script.js
+++ script.js
@@ -41,7 +41,25 @@ let cryptoKey;
 const queries = {
 
     /** @type {HTMLDivElement} */
-    page: document.getElementById('page')
+    page: document.getElementById('page'),
+
+    /** @type {HTMLDivElement} */
+    main: document.getElementById('main'),
+
+    /** @type {HTMLHeadingElement} */
+    subtitle: document.getElementById('main-subtitle'),
+
+    /** @type {HTMLDivElement} */
+    deriver: document.getElementById('deriver'),
+
+    /** @type {HTMLDivElement} */
+    uploader: document.getElementById('uploader'),
+
+    /** @type {HTMLDivElement} */
+    default: document.getElementById('default-option'),
+
+    /** @type {HTMLDivElement} */
+    debug: document.getElementById('debug-option')
 
 }
 
@@ -142,19 +160,21 @@ function downloadEnc(text, filename = 'data.enc') {
 }
 
 /**
- * Add a `<button>` element to the page
- * 
- * @param {string} text Text to show on the button
- * @param {(event: MouseEvent) => void} func - Function to call when clicked
- * @returns {HTMLButtonElement} The `<button>` element
+ * Show the deriver `<div id="deriver">` and hide the uploader
  */
-function addButton(text, func) {
-    const button = document.createElement('button');
-    button.title = text;
-    button.textContent = text;
-    button.onclick = func;
-    queries.page.appendChild(button);
-    return button;
+function showDeriver() {
+    queries.deriver.classList.remove('hidden');
+    queries.uploader.classList.add('hidden');
+    queries.subtitle.textContent = 'Select an encryption key';
+}
+
+/**
+ * Show the deriver `<div id="uploader">` and hide the deriver
+ */
+function showUploader() {
+    queries.uploader.classList.remove('hidden');
+    queries.deriver.classList.add('hidden');
+    queries.subtitle.textContent = 'Upload a file';
 }
 
 // ~ ======================================================
@@ -164,61 +184,73 @@ function addButton(text, func) {
 // ? Run callback when all resources have loaded
 window.addEventListener('load', async () => {
 
+    // Show the deriver for generating keys
+    showDeriver();
+
     // < ======================================================
     // < Buttons
     // < ======================================================
 
-    const encryptButton = addButton('json-enc', () => {
-
-        // Convert a raw `.json` file to an encrypted `.enc` file and download
-        uploadFile(async (file) => {
-            const rawObject = JSON.parse(await file.text());
-            const encryptedString = await encryptObjectToString(rawObject, cryptoKey);
-            downloadEnc(encryptedString);
-        }, '.json');
-
-    });
-    encryptButton.classList.add('hidden');
-
-    const decryptButton = addButton('enc-json', () => {
-
-        // Convert an encrypted `.enc` file to a raw `.json` file and download
-        uploadFile(async (file) => {
-            const encryptedString = await file.text();
-            const rawObject = await decryptStringToObject(encryptedString, cryptoKey);
-            downloadJson(rawObject);
-        }, '.enc');
-
-    });
-    decryptButton.classList.add('hidden');
-
-    const defaultKeyButton = addButton('default-key', async () => {
-
-        // Derive `CryptoKey` object from default values
-        cryptoKey = await createKey(true);
-
-        // Switch the visible button set
-        defaultKeyButton.classList.add('hidden');
-        customKeyButton.classList.add('hidden');
-        encryptButton.classList.remove('hidden');
-        decryptButton.classList.remove('hidden');
-
-    });
-
-    const customKeyButton = addButton('pbkdf2-key', async () => {
+    // Add click functionality to the default key button
+    queries.default.addEventListener('click', async () => {
 
         // Derive `CryptoKey` object from custom user input
         cryptoKey = await createKey(false);
 
-        // Switch the visible button set
-        defaultKeyButton.classList.add('hidden');
-        customKeyButton.classList.add('hidden');
-        encryptButton.classList.remove('hidden');
-        decryptButton.classList.remove('hidden');
+        // Show the uploader and hide the deriver
+        showUploader();
+
+    });
+
+    // Add click functionality to the debug key button
+    queries.debug.addEventListener('click', async () => {
+
+        // Derive `CryptoKey` object from default values
+        cryptoKey = await createKey(true);
+
+        // Show the uploader and hide the deriver
+        showUploader();
+
+    });
+
+    // Add click functionality to the uploader
+    queries.uploader.addEventListener('click', () => {
+
+        uploadFile(async (file) => {
+
+            // Check file extension of uploaded file
+            const extension = file.name.split('.').pop().toLowerCase();
+
+            if (extension === 'json') {
+
+                // Convert raw `.json` file to an encrypted `.enc` file and download
+                const rawObject = JSON.parse(await file.text());
+                const encryptedString = await encryptObjectToString(rawObject, cryptoKey);
+                downloadEnc(encryptedString);
+
+            } else if (extension === 'enc') {
+
+                // Convert encrypted `.enc` file to a raw `.json` file and download
+                const encryptedString = await file.text();
+                try {
+                    const rawObject = await decryptStringToObject(encryptedString, cryptoKey);
+                    downloadJson(rawObject);
+                } catch (error) {
+
+                    // Warn user if there is a key mismatch
+                    console.warn('Current key does not match original encryption key');
+                    queries.uploader.classList.add('error');
+                    setTimeout(() => { queries.uploader.classList.remove('error') }, 1000);
+
+                }
+
+            }
+
+        }, '.enc,.json');
 
     });
 
     // Show the page element
-    queries.page.style.display = '';
+    queries.page.classList.remove('hidden');
 
 });
